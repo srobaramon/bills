ec064bc0c8c139990e01e8659e9876f16aca4b9c
Sroba, Roman <Roman.Sroba@garrettmotion.com>
2022-12-01 12:18:56 +0100

    main: Interactive table moved into script

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
 import pandas as pd
 from datetime import datetime, date, timedelta, time
 import streamlit as st
-from interactive_table import aggrid_interactive_table
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
+from st_aggrid.shared import GridUpdateMode
 from streamlit_pandas_profiling import st_profile_report
 from pandas_profiling import ProfileReport
+
+def aggrid_interactive_table(df: pd.DataFrame):
+    """Creates an st-aggrid interactive table based on a dataframe.
+
+    Args:
+        df (pd.DataFrame]): Source dataframe
+
+    Returns:
+        dict: The selected row
+    """
+
+    options = GridOptionsBuilder.from_dataframe(
+        df, enableRowGroup=True, enableValue=True, enablePivot=True
+    )
+
+    options.configure_side_bar()
+
+    options.configure_selection("multiple", use_checkbox=True, header_checkbox=True)
+    options.configure_pagination(enabled=True, paginationAutoPageSize=True)
+    selection = AgGrid(
+        df,
+        enable_enterprise_modules=True,
+        gridOptions=options.build(),
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        fit_columns_on_grid_load=False,
+        header_checkbox_selection_filtered_only=True,
+        allow_unsafe_jscode=True,
+        use_checkbox=True,
+        height=500,
+    )
+
+    return selection
 
 @st.cache(allow_output_mutation=True)
 def read_data(file):
